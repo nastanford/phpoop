71a655f5a18ee290f6eb71ef9e6d7e8ceaf56a20
Nathan Stanford <Nathan.Stanford@gmail.com>
2022-08-06 10:52:24 -0400

    remove comments

--- index.php
+++ index.php
@@ -1,44 +1,7 @@
 <?php
-
-
 spl_autoload_register(function ($class_name) {
     include 'classes/'.$class_name . '.class.php';
 });
-
-// $obj  = new MyClass1();
-// $obj2 = new MyClass2(); 
-
-
-
-
-// create autoloader
-// function autoloader($class) { 
-//   include_once('classes/' . $class . '.class.php');
-
-// }
-
-// function loadModel($class) {
-//   $path = $DOCUMENT_ROOT . '/models/';
-//   require_once $path . $class .'.php';
-// }
-
-// function loadView($class) {
-// $path = $DOCUMENT_ROOT . '/views/';
-// require_once $path . $class .'.php';
-// }
-
-
-  // spl_autoload_register('myAutoloader');
-
-  // function myAutoloader($class) {
-  //   $path = 'classes/';
-  //   $extension = 'class.php';
-  //   $fullpath = $path . $class .$extension;
-
-  //   include_once $fullpath;
-  // }
-
-// include_once 'classes/person.class.php';
 
 ?>
 <!DOCTYPE html>
@@ -53,11 +16,6 @@ spl_autoload_register(function ($class_name) {
 <?php 
   $person = new person("Jim", "Sample", "25");
   echo $person->getPerson();
-
-
-  // $house = new house();
-  // echo $house->owner();
-
 
 ?>
 
